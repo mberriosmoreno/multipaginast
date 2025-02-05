d9e79473de6f51f963bf34f9f196f7c012bd62a0
mberriosmoreno <94083004+mberriosmoreno@users.noreply.github.com>
2025-02-05 15:02:00 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+import streamlit as st
+
+# --- CONFIGURACIÓN DE PÁGINAS ---
+about_page = st.Page(
+    "views/about_me.py",
+    title="Acerca de Mí",
+    icon=":material/account_circle:",
+    default=True,
+)
+project_1_page = st.Page(
+    "views/sales_dashboard.py",
+    title="Tablero de Ventas",
+    icon=":material/bar_chart:",
+)
+project_2_page = st.Page(
+    "views/chatbot.py",
+    title="Chat Bot",
+    icon=":material/smart_toy:",
+)
+
+# --- CONFIGURACIÓN DE NAVEGACIÓN [CON SECCIONES] ---
+pg = st.navigation(
+    {
+        "Información": [about_page],
+        "Proyectos": [project_1_page, project_2_page],
+    }
+)
+
+# --- ELEMENTOS COMPARTIDOS EN TODAS LAS PÁGINAS ---
+st.logo("assets/codingisfun_logo.png")
+st.sidebar.markdown("Hecho con ❤️ por [Sven](https://youtube.com/@codingisfun)")
+
+# --- EJECUTAR NAVEGACIÓN ---
+pg.run()
