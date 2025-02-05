5f5eb422d2505740a2da6addf5f13e93acdf28c5
mberriosmoreno <94083004+mberriosmoreno@users.noreply.github.com>
2025-02-05 15:24:09 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ import streamlit as st
 
 # --- CONFIGURACIÓN DE PÁGINAS ---
 about_page = st.Page(
-    "Pages/about_me.py",
+    "views/about_me.py",
     title="Acerca de Mí",
     icon=":material/account_circle:",
     default=True,
 )
 project_1_page = st.Page(
-    "Pages/sales_dashboard.py",
+    "views/sales_dashboard.py",
     title="Tablero de Ventas",
     icon=":material/bar_chart:",
 )
 project_2_page = st.Page(
-    "Pages/chatbot.py",
+    "views/chatbot.py",
     title="Chat Bot",
     icon=":material/smart_toy:",
 )
@@ -28,7 +28,7 @@ pg = st.navigation(
 
 # --- ELEMENTOS COMPARTIDOS EN TODAS LAS PÁGINAS ---
 st.logo("assets/logo.png")
-st.sidebar.markdown("Hecho con ❤️ por [Sven](https://youtube.com/@codingisfun)")
+st.sidebar.markdown("Hecho con ❤️ por [Michael Berríos](https://youtube.com/@codingisfun)")
 
 # --- EJECUTAR NAVEGACIÓN ---
 pg.run()
